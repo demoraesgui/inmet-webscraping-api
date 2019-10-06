52da34f4096149b18d323e1355f809dcfe26b239
Guilherme Vieira <guilhermevdm@gmail.com>
2019-10-06 11:13:56 -0300

    adding MetereologicalData on populate station command

--- stations/admin.py
+++ stations/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import MeteorologicalStation
+from .models import MeteorologicalStation, MeteorologicalData
 # Register your models here.
 
 admin.site.register(MeteorologicalStation)
+admin.site.register(MeteorologicalData)
--- stations/management/commands/populate_stations.py
+++ stations/management/commands/populate_stations.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from stations.webscraper import Webscraper
-from stations.models import MeteorologicalStation
+from stations.models import MeteorologicalStation, MeteorologicalData
+from datetime import datetime, timedelta
 
 
 class Command(BaseCommand):
@@ -10,6 +11,11 @@ class Command(BaseCommand):
 
         webscraper = Webscraper()
         webscraper.get_stations_info()
+        yesterday = (datetime.today() - timedelta(days=1)).strftime("%d/%m/%Y")
+        one_year_ago = (datetime.today() - timedelta(days=1)
+                        ).strftime("%d/%m/%Y")
+
+        print(yesterday, one_year_ago)
 
         for index in webscraper.stations:
 
@@ -23,3 +29,41 @@ class Command(BaseCommand):
             station.city = webscraper.stations[index]['label'].split(' - ')[1]
 
             station.save()
+            print(one_year_ago ,yesterday)
+
+            try:
+                station_data = webscraper.get_station_data(
+                    webscraper.stations[index]['url'], one_year_ago, yesterday)
+
+                for data_sample in station_data:
+
+                    try:
+                        meteorological_data_sample = MeteorologicalData()
+                        meteorological_data_sample.MeteorologicalStation = station
+
+                        meteorological_data_sample.T_max = float(
+                            data_sample['temp_max'])
+
+                        meteorological_data_sample.T_min = float(
+                            data_sample['temp_min'])
+                        meteorological_data_sample.RH_max = float(
+                            data_sample['umid_max'])
+                        meteorological_data_sample.RH_min = float(
+                            data_sample['umid_min'])
+                        meteorological_data_sample.Rn = float(
+                            data_sample['radiacao'])
+                        meteorological_data_sample.U = float(
+                            data_sample['vento_vel'])
+                        meteorological_data_sample.P = float(
+                            data_sample['pressao'])
+                        meteorological_data_sample.Ri = float(
+                            data_sample['precipitacao'])
+                        meteorological_data_sample.date = datetime.strptime(
+                            data_sample['data']+' '+data_sample['hora'], "%d/%m/%Y %H")
+                        meteorological_data_sample.save()
+
+                    except:
+                        print('Data not valid')
+
+            except:
+                print('No data on Station')
